3e0dccf141a53088c967fe3cc3b4ae077dd8f77b
Arpit Jha <arpit.jha@pkware.com>
2022-03-25 11:02:18 +0530

    Customer Management System(Basic V)

--- src/main/java/com/customerapp/cms/service/CustomerService.java
+++ src/main/java/com/customerapp/cms/service/CustomerService.java
@@ -11,21 +11,24 @@ public class CustomerService {
     private int customerIdCount = 1;
     private List<Customer> customerList = new CopyOnWriteArrayList<>();
 
+    //add customer
+
     public Customer addCustomer(Customer customer){
         customer.setCustomerId(customerIdCount);
         customerList.add(customer);
         customerIdCount++;
         return customer;
     }
-
+    //get all customers
     public List<Customer> getCustomers(){
         return customerList;
     }
 
+    //get a particular customer
     public Customer getCustomer(int customerId){
         return customerList.stream().filter(c -> c.getCustomerId()==customerId).findFirst().get();
     }
-
+    //update existing customer
     public Customer updateCustomer(int customerId, Customer customer){
         customerList.stream().forEach(c->{
             if(c.getCustomerId()==customerId){
@@ -38,7 +41,7 @@ public class CustomerService {
 
         return customerList.stream().filter(c -> c.getCustomerId()==customerId).findFirst().get();
     }
-
+    //delete a customer
     public void deleteCustomer(int customerId){
         customerList.stream().forEach(c -> {
             if(c.getCustomerId()==customerId){
